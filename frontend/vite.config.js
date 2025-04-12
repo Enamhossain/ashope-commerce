f2b86e020bd1ac18f8e9cc522d4d42a4980c516b
Enamhossain <bhaianam9@gmail.com>
2025-04-12 10:54:59 +0600

    frontend changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // Default Vite port
-    open: true, // Opens browser automatically
-  },
-});
+  base: '/', // or './' for relative paths if hosting in subfolder
+})
